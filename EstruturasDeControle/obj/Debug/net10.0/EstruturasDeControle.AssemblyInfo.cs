7a00a8113d5fea030342f9d58630820cf6766e1c
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-01-11 10:23:49 -0300

    Update EstruturasDeControle.AssemblyInfo.cs

--- EstruturasDeControle/obj/Debug/net10.0/EstruturasDeControle.AssemblyInfo.cs
+++ EstruturasDeControle/obj/Debug/net10.0/EstruturasDeControle.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EstruturasDeControle")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1c6c45343019217d6ed2865d47da628b7e6854ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("EstruturasDeControle")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EstruturasDeControle")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
