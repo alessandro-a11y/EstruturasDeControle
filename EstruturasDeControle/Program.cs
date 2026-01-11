96ceff1e21844e71feb980d4c481b64cc0dcac54
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-01-11 10:49:33 -0300

    Update Program.cs

--- EstruturasDeControle/Program.cs
+++ EstruturasDeControle/Program.cs
@@ -1,8 +1,30 @@
 ﻿using System;
-namespace EstruturasdeControle{
-    internal class Program{
-        static void Main(string[] args){
-            
+namespace EstruturasDeControle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.WriteLine("Executando o projeto 6 - Estruturas de Controle");
+            int idadeJoao = 16;
+            int quantidadePessoas = 2;
+            if (idadeJoao >= 18)
+            {
+                Console.WriteLine("João é maior de idade.");
+            }
+            else
+            {
+                if (quantidadePessoas >= 2)
+                {
+                    Console.WriteLine("João é menor de idade, mas está acompanhado.");
+                }
+                else
+                {
+                    Console.WriteLine("João é menor de idade e não está acompanhado.");
+                }
+            }
+            Console.WriteLine("Tecle enter para fechar...");
+            Console.ReadLine();
         }
     }
 }
