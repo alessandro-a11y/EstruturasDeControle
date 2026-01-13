d6782df6f4009d599fbe7022440f5e48cb8f50c6
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-01-12 22:10:41 -0300

    Update Program.cs

--- EstruturasDeControle/Program.cs
+++ EstruturasDeControle/Program.cs
@@ -1,26 +1,26 @@
-﻿using System;
-namespace EstruturasDeControle
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Você é um cliente especial? (S/N):\n");
-            string resposta = Console.ReadLine().ToUpper();
+﻿//using System;
+//namespace EstruturasDeControle
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Você é um cliente especial? (S/N):\n");
+//            string resposta = Console.ReadLine().ToUpper();
 
-            if(resposta == "S")
-            {
-                Console.WriteLine("\nDesconto de 10%");
-            } else if (resposta == "N")
-                {
-                Console.WriteLine("Sem desconto");
-            }
-            else
-            {
-                Console.WriteLine("Você digitou um caractere inválido!!\n");
-            }
-                Console.WriteLine("\nFIM DO PROCESSAMENTO");
-            Console.ReadKey();
-        }
-    }
-}
+//            if(resposta == "S")
+//            {
+//                Console.WriteLine("\nDesconto de 10%");
+//            } else if (resposta == "N")
+//                {
+//                Console.WriteLine("Sem desconto");
+//            }
+//            else
+//            {
+//                Console.WriteLine("Você digitou um caractere inválido!!\n");
+//            }
+//                Console.WriteLine("\nFIM DO PROCESSAMENTO");
+//            Console.ReadKey();
+//        }
+//    }
+//}
