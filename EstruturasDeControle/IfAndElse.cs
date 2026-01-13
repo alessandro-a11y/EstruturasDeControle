d75b57b7b9188e47c71b382e4b52e4ca36a2751c
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-01-13 14:43:19 -0300

    Update IfAndElse.cs

--- EstruturasDeControle/IfAndElse.cs
+++ EstruturasDeControle/IfAndElse.cs
@@ -1,29 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Channels;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+//using System.Threading.Channels;
 
-namespace EstruturasDeControle
-{
-    internal class IfAndElse
-    {
-        public static void Main()
-        {
-            Console.WriteLine("Informe sua nota: \n");
-            double note = Convert.ToDouble(Console.ReadLine());
+//namespace EstruturasDeControle
+//{
+//    internal class IfAndElse
+//    {
+//        public static void Main()
+//        {
+//            Console.WriteLine("Informe sua nota: \n");
+//            double note = Convert.ToDouble(Console.ReadLine());
 
-            if (note < 0 || note > 10) 
-            {
-                Console.WriteLine("Nota inválida!");
-            }
-            else if (note >= 7)
-            {
-                Console.WriteLine("Aprovado!!");
-            }
-            else
-            {
-                Console.WriteLine("Reprovado!!");
-            }
-        }
-    }
-}
+//            if (note < 0 || note > 10) 
+//            {
+//                Console.WriteLine("Nota inválida!");
+//            }
+//            else if (note >= 7)
+//            {
+//                Console.WriteLine("Aprovado!!");
+//            }
+//            else
+//            {
+//                Console.WriteLine("Reprovado!!");
+//            }
+//        }
+//    }
+//}
