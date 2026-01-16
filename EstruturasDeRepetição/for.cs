c8140cd6de00fa8ada2d10e8c2f18826658de321
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-01-16 20:13:11 -0300

    Create for.cs

---
+++ EstruturasDeRepetição/for.cs
@@ -0,0 +1,38 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace EstruturasDeRepetição
+//{
+//    internal class @for
+//    {
+//        static void Main(string[] args)
+//        {
+//            while (true)
+//            {
+//                Console.WriteLine("Informe um número inteiro: \n");
+//                int numero = Convert.ToInt32(Console.ReadLine());
+//                if (numero < 0)
+//                {
+//                    Console.WriteLine("Número inválido! Tente novamente. \n");
+//                    continue;
+//                }
+//                else
+//                {
+//                    for (int i = 1; i <= 10; i++)
+//                    {
+//                        int resultado = numero * i;
+//                        Console.WriteLine($"{numero} x {i} = {resultado}");
+//                    }
+//                    Console.WriteLine("Deseja continuar a tabuada? s/n \n ");
+//                    string resposta = Console.ReadLine().ToLower();
+//                    if (resposta != "s")
+//                    {
+//                        break;
+//                    }
+//                }
+//            }
+//            Console.WriteLine("Fim do processamento!");
+//        }
+//    }
+//}
