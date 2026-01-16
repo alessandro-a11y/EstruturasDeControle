9839cdc0c15e66f1e1fd735f332c603aa563b64d
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-01-16 20:13:07 -0300

    Create for2.cs

---
+++ EstruturasDeRepetição/for2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EstruturasDeRepetição
+{
+    internal class for2
+    {
+        static void Main(string[] args)
+        {
+            for (int x = 0; x<5; x++)
+            {
+                for (int y = 0; y<5; y++)
+                {
+                    Console.WriteLine($"({x},{y})");
+                }
+            }
+        }
+    }
+}
+//// multiplas expressôes
+//for (int i = 1, j = 1; i + j <= 20; i++, j++)
+
+//{
+//    Console.WriteLine($"Somando indices: {i} + {j} = {i+j}");
+//}
