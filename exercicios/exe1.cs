794942af38be74efba9f85198b804f55ecb3c7f2
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-01-17 20:03:24 -0300

    Update exe1.cs

--- exercicios/exe1.cs
+++ exercicios/exe1.cs
@@ -1,26 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
 
-namespace exercicios
-{
-    internal class Exe1 // Corrigido para PascalCase conforme regra de nomenclatura
-    {
-        static void Main(string[] args)
+//namespace exercicios
+//{
+//    internal class Exe1 // Corrigido para PascalCase conforme regra de nomenclatura
+//    {
+//        static void Main(string[] args)
 
-        {
-            int number = 1;
-            int sum = 0;
-            do
-            {
-                Console.WriteLine($"Os 10 primeiros números são: {number}");
-                sum += number;
-                number++;
-            } while (number <= 10);
-            Console.WriteLine($"A soma dos números é: {sum}");
-        }
-    }
-}
+//        {
+//            int number = 1;
+//            int sum = 0;
+//            do
+//            {
+//                Console.WriteLine($"Os 10 primeiros números são: {number}");
+//                sum += number;
+//                number++;
+//            } while (number <= 10);
+//            Console.WriteLine($"A soma dos números é: {sum}");
+//        }
+//    }
+//}
 // While
 //int number = 1; // Corrigido para variável única e nome em inglês
 //int sum = 0; // Variável 'sum' declarada mas não utilizada
