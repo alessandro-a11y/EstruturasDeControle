75ca54c38978da99dcc6fab62b361481809479de
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-01-16 22:09:59 -0300

    Create Program.cs

---
+++ exercicios/Program.cs
@@ -0,0 +1,33 @@
+﻿//using System;
+//namespace exercicios
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Digite um número: \n");
+//            int firstNumber = Convert.ToInt32(Console.ReadLine());
+//            Console.WriteLine("Digite outro número: \n");
+//            int secondNumber = Convert.ToInt32(Console.ReadLine());
+//            Console.WriteLine("Digite o terceiro: \n");
+//            int thirdNumber = Convert.ToInt32(Console.ReadLine());
+
+//            if (firstNumber > secondNumber && firstNumber > thirdNumber)
+//            {
+//                Console.WriteLine($"O {firstNumber} é o maior");
+//            }
+//            else if (secondNumber > firstNumber && secondNumber > thirdNumber)
+//            {
+//                Console.WriteLine($"O {secondNumber} é o maior");
+//            }
+//            else if (thirdNumber > firstNumber && thirdNumber > secondNumber)
+//            {
+//                Console.WriteLine($"O {thirdNumber} é o maior");
+//            }
+//            else
+//            {
+//                Console.WriteLine("Há números iguais");
+//            }
+//        }
+//    }
+//}
