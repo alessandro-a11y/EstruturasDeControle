bcac5976e3636de4938025e6e02fc650b1ac6af7
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-01-17 20:03:28 -0300

    Create exe2.cs

---
+++ exercicios/exe2.cs
@@ -0,0 +1,37 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace exercicios
+//{
+//    internal class exe2
+//    {
+//        static void Main(string[] args)
+//        {
+//            while (true)
+//            {
+//                Console.WriteLine("Digite um número maior que 0 e ele será multiplicado: \n");
+//                int number = int.Parse(Console.ReadLine());
+
+//                if (number > 0)
+//                {
+//                    for (int i = 1; i <= 10; i++)
+//                    {
+//                        int result = number * i;
+//                        Console.WriteLine($"{number} x {i} = {result}");
+//                    }
+//                }
+//                else
+//                {
+//                    Console.WriteLine("Número inválido. Por favor, tente novamente.\n");
+//                }
+//                Console.WriteLine("Deseja continuar? s/n");
+//                string resposta = Console.ReadLine().ToLower();
+//                if (resposta != "s")
+//                {
+//                    break;
+//                }
+//            }
+//        }
+//    }
+//}
