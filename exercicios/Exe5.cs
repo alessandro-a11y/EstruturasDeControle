c6f9dd0184f8f1afb57ca24b32f1a1aa9304506e
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-01-17 20:03:44 -0300

    Create Exe5.cs

---
+++ exercicios/Exe5.cs
@@ -0,0 +1,23 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace exercicios
+//{
+//    internal class Exe5
+//    {
+//        static void Main(string[] args)
+//        {
+//            // triangulo reto
+            
+//            for (int i = 1; i <= 4; i++)
+//            {
+//                for (int j = 1; j <= i; j++)
+//                {
+//                    Console.Write("*");
+//                }
+//                Console.WriteLine();
+//            }
+//        }
+//    }
+//}
