eb1a332a4c529c223d777625e9f4d23c2bdbf001
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-01-17 20:03:54 -0300

    Create Exe7.cs

---
+++ exercicios/Exe7.cs
@@ -0,0 +1,33 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace exercicios
+//{
+//    internal class Exe7
+//    {
+//        static void Main(string[] args) 
+//        {
+//            /*
+//             Escreva um programa para exibir as tabelas de multiplicação do 2 ao 6 usando o loop do-while aninhado
+//             */
+//            int i = 1;
+//            do
+//            { 
+//             Console.WriteLine($"Tabela de multiplicação do {i}:");
+             
+//                    int j = 1;
+//                do 
+//                {
+//                  Console.WriteLine($"{i} x {j} = {i * j}");
+//                    j++;
+
+//                } while (j <=10);
+//                    Console.WriteLine();
+//                    i++;
+//            } while (i <= 6);
+//        } 
+//    }
+//}
+
+
