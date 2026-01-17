38cc9664f38ba87a202ecb39d9df2de1fdf0d53f
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-01-17 20:03:58 -0300

    Create Exe8.cs

---
+++ exercicios/Exe8.cs
@@ -0,0 +1,51 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+
+//namespace exercicios
+//{
+//    internal class Exe8
+//    {
+//        static void Main(string[] args)
+//        {
+//            /*Crie um programa  que recebe na entrada de dados um número inteiro de 0 a 10 que representa a nota de 
+//             um aluno. Com base na tabela a seguir imprima no console qual o resultado da avaliação do aluno. (Use a 
+//             instrução switch, break e default em um loop infinito e defina uma condição de saida.)
+//            */
+
+//            Console.WriteLine("Digite sua nota de 0 a 10");
+//            int note = int.Parse(Console.ReadLine());
+//            switch (note)
+//            {
+//                case 0:
+//                case 1:
+//                case 2:
+//                case 4:
+//                    Console.WriteLine("F");
+//                    break;
+//                case 5:
+//                    Console.WriteLine("E");
+//                    break;
+//                case 6:
+//                    Console.WriteLine("C");
+//                    break;
+//                case 7:
+//                case 8:
+//                    Console.WriteLine("B");
+//                    break;
+//                case 9:
+//                    Console.WriteLine("A");
+//                    break;
+//                case 10:
+//                    Console.WriteLine("A+");
+//                    break;
+//                default:
+//                    Console.WriteLine("Digite um número válido");
+//                    break;
+//            }
+
+//        }
+//    }
+//}
+
+
