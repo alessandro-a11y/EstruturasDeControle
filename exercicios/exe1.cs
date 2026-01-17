74a5fc9bee30c2447c107da1d5ed713d3ba1f101
Alessandro Figueiredo Rodrigues <figueiredoalessandro73@gmail.com>
2026-01-16 22:10:54 -0300

    Create exe1.cs

---
+++ exercicios/exe1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace exercicios
+{
+    internal class Exe1 // Corrigido para PascalCase conforme regra de nomenclatura
+    {
+        static void Main(string[] args)
+
+        {
+            int number = 1;
+            int sum = 0;
+            do
+            {
+                Console.WriteLine($"Os 10 primeiros números são: {number}");
+                sum += number;
+                number++;
+            } while (number <= 10);
+            Console.WriteLine($"A soma dos números é: {sum}");
+        }
+    }
+}
+// While
+//int number = 1; // Corrigido para variável única e nome em inglês
+//int sum = 0; // Variável 'sum' declarada mas não utilizada
+
+//while (number <= 10)
+//{
+//    Console.WriteLine($"Os 10 primeiros números são {number}");
+//    sum += number;
+//    number++;
+//}
+//Console.WriteLine($"A soma dos números é: {sum}");
+
+//// for 
+//int sum = 0; // Variável 'sum' declarada mas não utilizada
+//for (int number = 1; number <= 10; number++) // Usado 'for' para simplificar o loop
+//{
+//    Console.WriteLine($"Os 10 primeiros números são {number}");
+//    sum += number; // Corrigido para somar corretamente
+//}
+//Console.WriteLine($"A soma dos números é: {sum}");
+//        }
+
+// do-while
